d0892390e9ce328127fc25b5752678b286dd5046
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2023-08-25 23:31:55 +0900

    fix lock

--- torb/webapp/go/src/torb/app.go
+++ torb/webapp/go/src/torb/app.go
@@ -743,7 +743,7 @@ func main() {
 			if err != nil {
 				return err
 			}
-			if err := tx.QueryRow("SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE) AND `rank` = ? ORDER BY RAND() LIMIT 1", event.ID, params.Rank).Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
+			if err := tx.QueryRow("SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL) AND `rank` = ? ORDER BY RAND() LIMIT 1", event.ID, params.Rank).Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
 				if err == sql.ErrNoRows {
 					tx.Rollback()
 					return resError(c, "sold_out", 409)
