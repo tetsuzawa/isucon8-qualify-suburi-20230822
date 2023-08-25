ac7626abbe8f890f41b12e13e3c8e6bfe9f6651d
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2023-08-25 23:36:32 +0900

    fix

--- torb/webapp/go/src/torb/app.go
+++ torb/webapp/go/src/torb/app.go
@@ -752,6 +752,7 @@ func main() {
 				return err
 			}
 
+			c.Logger().Debugf("sheet: %v", sheet.ID)
 			if err := tx.QueryRow("SELECT * FROM sheets WHERE id = ? FOR UPDATE", sheet.ID).Err(); err != nil {
 				if err == sql.ErrNoRows {
 					tx.Rollback()
@@ -764,18 +765,18 @@ func main() {
 			res, err := tx.Exec("INSERT INTO reservations (event_id, sheet_id, user_id, reserved_at) VALUES (?, ?, ?, ?)", event.ID, sheet.ID, user.ID, time.Now().UTC().Format("2006-01-02 15:04:05.000000"))
 			if err != nil {
 				tx.Rollback()
-				log.Println("re-try: rollback by", err)
+				log.Println("re-try: 1 rollback by", err)
 				continue
 			}
 			reservationID, err = res.LastInsertId()
 			if err != nil {
 				tx.Rollback()
-				log.Println("re-try: rollback by", err)
+				log.Println("re-try: 2 rollback by", err)
 				continue
 			}
 			if err := tx.Commit(); err != nil {
 				tx.Rollback()
-				log.Println("re-try: rollback by", err)
+				log.Println("re-try: 3 rollback by", err)
 				continue
 			}
 
